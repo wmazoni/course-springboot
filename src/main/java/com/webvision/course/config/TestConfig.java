80ae0ff14571f75dd00465a1435bc3d4987f70cd
Wellington Andrade <wmazoni@protonmail.com>
2020-06-25 20:22:47 -0300

    OrderItem, many-to-many association with extra attributes PARTE 2

--- src/main/java/com/webvision/course/config/TestConfig.java
+++ src/main/java/com/webvision/course/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.webvision.course.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.webvision.course.entities.Category;
 import com.webvision.course.entities.Order;
+import com.webvision.course.entities.OrderItem;
 import com.webvision.course.entities.Product;
 import com.webvision.course.entities.User;
 import com.webvision.course.entities.enums.OrderStatus;
 import com.webvision.course.repositories.CategoryRepository;
+import com.webvision.course.repositories.OrderItemRepository;
 import com.webvision.course.repositories.OrderRepository;
 import com.webvision.course.repositories.ProductRepository;
 import com.webvision.course.repositories.UserRepository;
@@ -34,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemtRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,5 +73,12 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemtRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
